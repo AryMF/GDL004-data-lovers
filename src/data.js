02d007d0a811c2279b85dcdfbd5cef70c5d5c6a7
AryMF <ariadne.hp@gmail.com>
2019-11-13 10:42:20 -0600

    Funcionalidad despliegue de data en main screen

--- src/data.js
+++ src/data.js
@@ -2,6 +2,30 @@
 
 // esta es una función de ejemplo
 
-export const example = () => {
-  return 'example';
+// export const example = () => {
+//   return 'example';
+// };
+
+window.data =  {
+  filterData: function(data, condition){
+    let filterJSON = [];
+    let dataLength = Object.keys(data.pokemon); //Por si se necesita
+  
+
+    const filteredByNameOrNumber = (array) =>{
+      if(("name" in array && array.name == condition) || ("id" in array && array.id == condition)){
+        return true;
+      } else{
+        return false;
+      }
+    }    
+
+    filterJSON = data.pokemon.filter(filteredByNameOrNumber);
+
+    return filterJSON;
+
+  },
+  sortData: function(data, sortBy, sortOrder){
+
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,15 +1,66 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+const DATA_URL = "https://raw.githubusercontent.com/AryMF/GDL004-data-lovers/master/src/data/pokemon/pokemon.json";
+let dataJSON = {};
 
-import { example } from './data.js';
+async function getData (){
+    const dataRequest = await fetch(DATA_URL);
+    dataJSON = await dataRequest.json(); 
+    return dataJSON;
+};
 
-console.log(example);
+const main = ()  =>{
+    getData()
+        .then(dataJSON => {
+            console.log("Tu JSON obtenido por fetch es:");
+            console.log(dataJSON);
+            // let filterJSON = window.data.filterData(dataJSON, "Bulbasaur");
+            // console.log("Tu JSON filtrado es:");
+            //  console.log(filterJSON);
+            printPokemonCards(dataJSON.pokemon);
+        })
+        .catch(error => {
+            console.error("Error al cargar JSON por fetch");
+            console.log(error);
+        });
+};
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+window.addEventListener("load", main);
+
+const printPokemonCards = (dataArray) => {
+    let i=0;
+    let divContainer;
+    let divPokemonCard;
+    let pokemonImage;
+    let pokemonName;
+    let randomColor;    
+
+    dataArray.forEach(element => {
+        divContainer = document.createElement("DIV");
+        // randomColor = "background-color: #" + Math.floor(Math.random()*16777215).toString(16) + ";";
+        // divContainer.setAttribute("style", randomColor);
+        divContainer.classList.add("divContainerClass");
+        document.getElementById("pokemonContainer").appendChild(divContainer);
+
+        divPokemonCard = document.createElement("DIV");
+        randomColor = "background-color: #" + Math.floor(Math.random()*16777215).toString(16) + ";";
+        divPokemonCard.setAttribute("style", randomColor);
+        divPokemonCard.classList.add("divPokemonCardClass");
+        divContainer.appendChild(divPokemonCard);
+
+        pokemonImage = document.createElement("IMG");
+        // pokemonImage.className = "imagen";
+        pokemonImage.setAttribute("id", element.id);
+        pokemonImage.setAttribute("src", element.img);
+        pokemonImage.setAttribute("alt", element.name);
+        pokemonImage.classList.add("imagePokemon");
+        pokemonImage.addEventListener("click", function() {
+            alert("Hola yo soy " + element.name);
+        });
+        divPokemonCard.appendChild(pokemonImage);
+
+        pokemonName = document.createElement("SPAN");
+        pokemonName.innerHTML = element.name;
+        pokemonName.classList.add("TextFormat");
+        divPokemonCard.appendChild(pokemonName);
+        
+    });
+}
